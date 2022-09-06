57838afa012c341fcf3221ce00b06746e56b99ab
Ved Tambat <ved.atultambat@harness.io>
2022-09-07 02:36:02 +0530

    [CDS-42743]: compile fix

--- 954-connector-beans/src/main/java/io/harness/delegate/beans/connector/azureartifacts/AzureArtifactsUsernameTokenDTO.java
+++ 954-connector-beans/src/main/java/io/harness/delegate/beans/connector/azureartifacts/AzureArtifactsUsernameTokenDTO.java
@@ -13,6 +13,11 @@ import io.harness.encryption.SecretRefData;
 import io.harness.encryption.SecretReference;
 import io.harness.validation.OneOfField;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
