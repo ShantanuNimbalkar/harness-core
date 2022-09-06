f0536337c7e92bdd62035054e49817697a38c8aa
Ved Tambat <ved.atultambat@harness.io>
2022-09-06 22:57:21 +0530

    [CDS-42743]: mapper changes

--- 440-connector-nextgen/src/main/java/io/harness/connector/mappers/azureartifacts/AzureArtifactsDTOToEntity.java
+++ 440-connector-nextgen/src/main/java/io/harness/connector/mappers/azureartifacts/AzureArtifactsDTOToEntity.java
@@ -7,14 +7,135 @@
 
 package io.harness.connector.mappers.azureartifacts;
 
-import io.harness.connector.entities.embedded.azureartifacts.AzureArtifactsConnector;
+import io.harness.connector.entities.embedded.azureartifacts.*;
 import io.harness.connector.mappers.ConnectorDTOToEntityMapper;
-import io.harness.delegate.beans.connector.azureartifacts.AzureArtifactsConnectorDTO;
+import io.harness.delegate.beans.connector.azureartifacts.*;
+import io.harness.delegate.beans.connector.scm.GitAuthType;
+import io.harness.encryption.SecretRefData;
+import io.harness.encryption.SecretRefHelper;
+import io.harness.exception.InvalidRequestException;
+import io.harness.exception.UnknownEnumTypeException;
 
 public class AzureArtifactsDTOToEntity
     implements ConnectorDTOToEntityMapper<AzureArtifactsConnectorDTO, AzureArtifactsConnector> {
   @Override
-  public AzureArtifactsConnector toConnectorEntity(AzureArtifactsConnectorDTO connectorDTO) {
-    return null;
+  public AzureArtifactsConnector toConnectorEntity(AzureArtifactsConnectorDTO configDTO) {
+    if (configDTO == null) {
+      throw new InvalidRequestException("AzureArtifacts Config DTO is not found");
+    }
+    if (configDTO.getAuthentication() == null) {
+      throw new InvalidRequestException("No Authentication Details Found in the connector");
+    }
+    GitAuthType gitAuthType = getAuthType(configDTO.getAuthentication());
+    AzureArtifactsAuthentication azureArtifactsAuthentication =
+        buildAuthenticationDetails(gitAuthType, configDTO.getAuthentication().getCredentials());
+    boolean hasApiAccess = hasApiAccess(configDTO.getApiAccess());
+    AzureArtifactsApiAccessType apiAccessType = null;
+    AzureArtifactsApiAccessDetails azureArtifactsApiAccess = null;
+    if (hasApiAccess) {
+      apiAccessType = getApiAccessType(configDTO.getApiAccess());
+      azureArtifactsApiAccess = getApiAcessByType(configDTO.getApiAccess().getSpec(), apiAccessType);
+    }
+    return AzureArtifactsConnector.builder()
+        .connectionType(getAzureArtifactsConnectionType(configDTO.getConnectionType()))
+        .authType(gitAuthType)
+        .hasApiAccess(hasApiAccess)
+        .apiAccessType(apiAccessType)
+        .authenticationDetails(azureArtifactsAuthentication)
+        .azureArtifactsApiAccess(azureArtifactsApiAccess)
+        .url(configDTO.getUrl())
+        .validationRepo(configDTO.getValidationRepo())
+        .build();
+  }
+
+  public AzureArtifactsAuthentication buildAuthenticationDetails(
+      GitAuthType gitAuthType, AzureArtifactsCredentialsDTO credentialsDTO) {
+    if (gitAuthType == null) {
+      throw new InvalidRequestException("Auth Type not found");
+    }
+    switch (gitAuthType) {
+      case SSH:
+        final AzureArtifactsSshCredentialsDTO sshCredentialsDTO = (AzureArtifactsSshCredentialsDTO) credentialsDTO;
+        return AzureArtifactsSshAuthentication.builder()
+            .sshKeyRef(SecretRefHelper.getSecretConfigString(sshCredentialsDTO.getSshKeyRef()))
+            .build();
+      case HTTP:
+        final AzureArtifactsHttpCredentialsDTO httpCredentialsDTO = (AzureArtifactsHttpCredentialsDTO) credentialsDTO;
+        final AzureArtifactsHttpAuthenticationType type = httpCredentialsDTO.getType();
+        return AzureArtifactsHttpAuthentication.builder()
+            .type(type)
+            .auth(getHttpAuth(type, httpCredentialsDTO))
+            .build();
+      default:
+        throw new UnknownEnumTypeException("AzureArtifacts Auth Type ", gitAuthType.getDisplayName());
+    }
+  }
+
+  private AzureArtifactsHttpAuth getHttpAuth(
+      AzureArtifactsHttpAuthenticationType type, AzureArtifactsHttpCredentialsDTO httpCredentialsDTO) {
+    if (type == null) {
+      throw new InvalidRequestException("AzureArtifacts Http Auth Type not found");
+    }
+    switch (type) {
+      case USERNAME_AND_TOKEN:
+        final AzureArtifactsUsernameTokenDTO azureArtifactsUsernameTokenDTO =
+            (AzureArtifactsUsernameTokenDTO) httpCredentialsDTO.getHttpCredentialsSpec();
+        String usernameReference = getUsernameRefFromSecret(azureArtifactsUsernameTokenDTO.getUsernameRef());
+        return AzureArtifactsUsernameToken.builder()
+            .tokenRef(SecretRefHelper.getSecretConfigString(azureArtifactsUsernameTokenDTO.getTokenRef()))
+            .username(azureArtifactsUsernameTokenDTO.getUsername())
+            .usernameRef(usernameReference)
+            .build();
+      default:
+        throw new UnknownEnumTypeException("AzureArtifacts Http Auth Type ", type.getDisplayName());
+    }
+  }
+
+  private String getUsernameRefFromSecret(SecretRefData secretRefData) {
+    String usernameRef = null;
+    if (secretRefData != null) {
+      usernameRef = SecretRefHelper.getSecretConfigString(secretRefData);
+    }
+    return usernameRef;
+  }
+
+  private AzureArtifactsApiAccess getApiAcessByType(
+      AzureArtifactsApiAccessSpecDTO spec, AzureArtifactsApiAccessType apiAccessType) {
+    if (apiAccessType == null) {
+      throw new InvalidRequestException("AzureArtifacts Api Access Type not found");
+    }
+    switch (apiAccessType) {
+      case TOKEN:
+        final AzureArtifactsTokenSpecDTO tokenSpec = (AzureArtifactsTokenSpecDTO) spec;
+        return AzureArtifactsTokenApiAccess.builder()
+            .tokenRef(SecretRefHelper.getSecretConfigString(tokenSpec.getTokenRef()))
+            .build();
+      default:
+        throw new UnknownEnumTypeException("AzureArtifacts Api Access Type ", apiAccessType.getDisplayName());
+    }
+  }
+
+  private AzureArtifactsApiAccessType getApiAccessType(AzureArtifactsApiAccessDTO apiAccess) {
+    return apiAccess.getType();
+  }
+
+  private boolean hasApiAccess(AzureArtifactsApiAccessDTO apiAccess) {
+    return apiAccess != null;
+  }
+
+  private GitAuthType getAuthType(AzureArtifactsAuthenticationDTO authentication) {
+    return authentication.getAuthType();
+  }
+
+  @VisibleForTesting
+  public AzureArtifactsConnectionType getAzureArtifactsConnectionType(AzureArtifactsConnectionTypeDTO connectionType) {
+    switch (connectionType) {
+      case PROJECT:
+        return AzureArtifactsConnectionType.PROJECT;
+      case REPO:
+        return AzureArtifactsConnectionType.REPO;
+      default:
+        throw new UnknownEnumTypeException("AzureArtifacts Connection Type ", connectionType.name());
+    }
   }
 }
--- 440-connector-nextgen/src/main/java/io/harness/connector/mappers/azureartifacts/AzureArtifactsEntityToDTO.java
+++ 440-connector-nextgen/src/main/java/io/harness/connector/mappers/azureartifacts/AzureArtifactsEntityToDTO.java
@@ -7,14 +7,126 @@
 
 package io.harness.connector.mappers.azureartifacts;
 
-import io.harness.connector.entities.embedded.azureartifacts.AzureArtifactsConnector;
+import io.harness.annotations.dev.HarnessTeam;
+import io.harness.annotations.dev.OwnedBy;
+import io.harness.connector.entities.embedded.azureartifacts.*;
 import io.harness.connector.mappers.ConnectorEntityToDTOMapper;
-import io.harness.delegate.beans.connector.azureartifacts.AzureArtifactsConnectorDTO;
+import io.harness.delegate.beans.connector.azureartifacts.*;
+import io.harness.delegate.beans.connector.scm.GitAuthType;
+import io.harness.encryption.SecretRefData;
+import io.harness.encryption.SecretRefHelper;
+import io.harness.exception.InvalidRequestException;
+import io.harness.exception.UnknownEnumTypeException;
 
+@OwnedBy(HarnessTeam.CDC)
 public class AzureArtifactsEntityToDTO
     implements ConnectorEntityToDTOMapper<AzureArtifactsConnectorDTO, AzureArtifactsConnector> {
   @Override
   public AzureArtifactsConnectorDTO createConnectorDTO(AzureArtifactsConnector connector) {
-    return null;
+    if (connector == null) {
+      throw new InvalidRequestException("Connector object not found");
+    }
+    AzureArtifactsAuthenticationDTO azureAuthenticationDTO =
+        buildAzureArtifactsAuthentication(connector.getAuthType(), connector.getAuthenticationDetails());
+    AzureArtifactsApiAccessDTO azureApiAccess = null;
+    if (connector.isHasApiAccess()) {
+      azureApiAccess = buildApiAccess(connector);
+    }
+    return AzureArtifactsConnectorDTO.builder()
+        .apiAccess(azureApiAccess)
+        .connectionType(getAzureArtifactsConnectionTypeDTO(connector.getConnectionType()))
+        .authentication(azureAuthenticationDTO)
+        .url(connector.getUrl())
+        .validationRepo(connector.getValidationRepo())
+        .build();
+  }
+
+  public AzureArtifactsAuthenticationDTO buildAzureArtifactsAuthentication(
+      GitAuthType authType, AzureArtifactsAuthentication authenticationDetails) {
+    AzureArtifactsCredentialsDTO azureCredentialsDTO = null;
+    if (authType == null) {
+      throw new InvalidRequestException("AzureArtifacts Auth Type not found");
+    }
+    switch (authType) {
+      case SSH:
+        final AzureArtifactsSshAuthentication azureSshAuthentication =
+            (AzureArtifactsSshAuthentication) authenticationDetails;
+        azureCredentialsDTO = AzureArtifactsSshCredentialsDTO.builder()
+                                  .sshKeyRef(SecretRefHelper.createSecretRef(azureSshAuthentication.getSshKeyRef()))
+                                  .build();
+        break;
+      case HTTP:
+        final AzureArtifactsHttpAuthentication azureHttpAuthentication =
+            (AzureArtifactsHttpAuthentication) authenticationDetails;
+        final AzureArtifactsHttpAuthenticationType type = azureHttpAuthentication.getType();
+        final AzureArtifactsHttpAuth auth = azureHttpAuthentication.getAuth();
+        AzureArtifactsHttpCredentialsSpecDTO azureHttpCredentialsSpecDTO = getHttpCredentialsSpecDTO(type, auth);
+        azureCredentialsDTO = AzureArtifactsHttpCredentialsDTO.builder()
+                                  .type(type)
+                                  .httpCredentialsSpec(azureHttpCredentialsSpecDTO)
+                                  .build();
+        break;
+      default:
+        throw new UnknownEnumTypeException("AzureArtifacts Auth Type", authType.getDisplayName());
+    }
+    return AzureArtifactsAuthenticationDTO.builder().authType(authType).credentials(azureCredentialsDTO).build();
+  }
+
+  private AzureArtifactsHttpCredentialsSpecDTO getHttpCredentialsSpecDTO(
+      AzureArtifactsHttpAuthenticationType type, Object auth) {
+    AzureArtifactsHttpCredentialsSpecDTO azureHttpCredentialsSpecDTO = null;
+    if (type == null) {
+      throw new InvalidRequestException("AzureArtifacts Http Auth Type not found");
+    }
+    switch (type) {
+      case USERNAME_AND_TOKEN:
+        final AzureArtifactsUsernameToken usernameToken = (AzureArtifactsUsernameToken) auth;
+        SecretRefData usernameReference = null;
+        if (usernameToken.getUsernameRef() != null) {
+          usernameReference = SecretRefHelper.createSecretRef(usernameToken.getUsernameRef());
+        }
+        azureHttpCredentialsSpecDTO = AzureArtifactsUsernameTokenDTO.builder()
+                                          .username(usernameToken.getUsername())
+                                          .usernameRef(usernameReference)
+                                          .tokenRef(SecretRefHelper.createSecretRef(usernameToken.getTokenRef()))
+                                          .build();
+        break;
+      default:
+        throw new UnknownEnumTypeException("AzureArtifacts Http Auth Type", type.getDisplayName());
+    }
+    return azureHttpCredentialsSpecDTO;
+  }
+
+  private AzureArtifactsApiAccessDTO buildApiAccess(AzureArtifactsConnector connector) {
+    final AzureArtifactsApiAccessType apiAccessType = connector.getApiAccessType();
+    AzureArtifactsApiAccessSpecDTO apiAccessSpecDTO = null;
+    if (apiAccessType == null) {
+      throw new InvalidRequestException("AzureArtifacts Api Access Type not found");
+    }
+    switch (apiAccessType) {
+      case TOKEN:
+        final AzureArtifactsTokenApiAccess azureTokenApiAccess =
+            (AzureArtifactsTokenApiAccess) connector.getAzureArtifactsApiAccess();
+        apiAccessSpecDTO = AzureArtifactsTokenSpecDTO.builder()
+                               .tokenRef(SecretRefHelper.createSecretRef(azureTokenApiAccess.getTokenRef()))
+                               .build();
+        break;
+      default:
+        throw new UnknownEnumTypeException("AzureArtifacts Api Access Type", apiAccessType.getDisplayName());
+    }
+    return AzureArtifactsApiAccessDTO.builder().type(apiAccessType).spec(apiAccessSpecDTO).build();
+  }
+
+  @VisibleForTesting
+  public AzureArtifactsConnectionTypeDTO getAzureArtifactsConnectionTypeDTO(
+      AzureArtifactsConnectionType connectionType) {
+    switch (connectionType) {
+      case PROJECT:
+        return AzureArtifactsConnectionTypeDTO.PROJECT;
+      case REPO:
+        return AzureArtifactsConnectionTypeDTO.REPO;
+      default:
+        throw new UnknownEnumTypeException("AzureArtifacts Connection Type ", connectionType.name());
+    }
   }
 }
