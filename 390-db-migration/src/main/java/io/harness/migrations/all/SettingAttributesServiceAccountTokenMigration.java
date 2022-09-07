5376b61ce47d0ce8b878484cbbd97442fe2a4632
Tejas Tiwari <tejas.tiwari@harness.io>
2022-09-07 11:27:39 +0530

    [PL-26945]: Resolved PR check failures

--- 390-db-migration/src/main/java/io/harness/migrations/all/SettingAttributesServiceAccountTokenMigration.java
+++ 390-db-migration/src/main/java/io/harness/migrations/all/SettingAttributesServiceAccountTokenMigration.java
@@ -1,6 +1,9 @@
 package io.harness.migrations.all;
 
-import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+import static io.harness.beans.EncryptedData.EncryptedDataKeys;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+
+import static software.wings.beans.SettingAttribute.SettingAttributeKeys;
 
 import io.harness.beans.EncryptedData;
 import io.harness.k8s.model.KubernetesClusterAuthType;
@@ -13,14 +16,12 @@ import software.wings.beans.SettingAttribute;
 import software.wings.dl.WingsPersistence;
 
 import com.google.inject.Inject;
-
-import lombok.extern.slf4j.Slf4j;
-import org.mongodb.morphia.query.UpdateOperations;
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.mongodb.morphia.query.UpdateOperations;
 
 @Slf4j
 public class SettingAttributesServiceAccountTokenMigration implements Migration {
@@ -33,17 +34,15 @@ public class SettingAttributesServiceAccountTokenMigration implements Migration 
         "yTbabkw4SdCPZdnEWynUNg"; // Account Id for the customer- "Kroger" for whom the migration is required
     Set<String> tokenIds = new HashSet<>();
 
-    try(HIterator<SettingAttribute> settingAttributes = new HIterator<>(
-        wingsPersistence.createQuery(SettingAttribute.class)
-            .filter(SettingAttribute.SettingAttributeKeys.accountId, accountId)
-            .filter(SettingAttribute.SettingAttributeKeys.category, SettingAttribute.SettingCategory.CLOUD_PROVIDER)
-            .filter(SettingAttribute.SettingAttributeKeys.value_type, "KUBERNETES_CLUSTER")
-            .fetch());
-    HIterator<EncryptedData> encryptedRecords =
-        new HIterator<>(wingsPersistence.createQuery(EncryptedData.class)
-                            .filter(EncryptedData.EncryptedDataKeys.accountId, accountId)
-                            .fetch());
-    ) {
+    try (HIterator<SettingAttribute> settingAttributes =
+             new HIterator<>(wingsPersistence.createQuery(SettingAttribute.class)
+                                 .filter(SettingAttributeKeys.accountId, accountId)
+                                 .filter(SettingAttributeKeys.category, SettingAttribute.SettingCategory.CLOUD_PROVIDER)
+                                 .filter(SettingAttributeKeys.value_type, "KUBERNETES_CLUSTER")
+                                 .fetch());
+         HIterator<EncryptedData> encryptedRecords = new HIterator<>(wingsPersistence.createQuery(EncryptedData.class)
+                                                                         .filter(EncryptedDataKeys.accountId, accountId)
+                                                                         .fetch());) {
       Map<String, String> tokenIdsMap = new HashMap<>();
       while (encryptedRecords.hasNext()) {
         EncryptedData encryptedData = encryptedRecords.next();
@@ -53,7 +52,7 @@ public class SettingAttributesServiceAccountTokenMigration implements Migration 
       while (settingAttributes.hasNext()) {
         SettingAttribute settingAttribute = settingAttributes.next();
         if (((KubernetesClusterConfig) settingAttribute.getValue()).getAuthType()
-                != KubernetesClusterAuthType.SERVICE_ACCOUNT) {
+            != KubernetesClusterAuthType.SERVICE_ACCOUNT) {
           continue;
         }
         if (tokenIds.contains(
@@ -61,7 +60,8 @@ public class SettingAttributesServiceAccountTokenMigration implements Migration 
           continue;
         }
         UpdateOperations<SettingAttribute> ops = mongoPersistence.createUpdateOperations(SettingAttribute.class);
-        String currentToken = tokenIdsMap.get(((KubernetesClusterConfig) settingAttribute.getValue()).getEncryptedServiceAccountToken());
+        String currentToken =
+            tokenIdsMap.get(((KubernetesClusterConfig) settingAttribute.getValue()).getEncryptedServiceAccountToken());
         if (isNotEmpty(currentToken)) {
           ops.set("value.encryptedServiceAccountToken", currentToken);
           mongoPersistence.update(settingAttribute, ops);
