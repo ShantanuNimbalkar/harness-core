43cdb639e809ae6295cfc13c099bb493463e1093
Brijesh Dhakar <brijesh.dhakar@harness.io>
2022-09-09 02:49:09 +0530

    minor change

--- clients/pipeline-service/pms-sdk-core/src/main/java/io/harness/pms/sdk/core/execution/AsyncSdkTimeoutCallback.java
+++ clients/pipeline-service/pms-sdk-core/src/main/java/io/harness/pms/sdk/core/execution/AsyncSdkTimeoutCallback.java
@@ -11,10 +11,12 @@ import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
 
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.pms.contracts.ambiance.Ambiance;
+import io.harness.pms.execution.utils.AmbianceUtils;
 import io.harness.tasks.ResponseData;
 import io.harness.waiter.OldNotifyCallback;
 
 import com.google.inject.Inject;
+import com.google.protobuf.InvalidProtocolBufferException;
 import java.util.Map;
 import lombok.Builder;
 import lombok.Data;
@@ -22,8 +24,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Data
 @OwnedBy(PIPELINE)
-@Slf4j
 @Builder
+@Slf4j
 public class AsyncSdkTimeoutCallback implements OldNotifyCallback {
   @Inject SdkNodeExecutionService sdkNodeExecutionService;
   byte[] ambianceBytes;
@@ -45,7 +47,13 @@ public class AsyncSdkTimeoutCallback implements OldNotifyCallback {
 
   // TODO: Implement the methods.
   private void notifyResponse(Map<String, ResponseData> responseMap, boolean timedOut) {
-    Ambiance ambiance = Ambiance.parseFrom(ambianceBytes);
-    sdkNodeExecutionService.resumeNodeExecution(ambiance, responseMap, false, timedOut);
+    try {
+      Ambiance ambiance = Ambiance.parseFrom(ambianceBytes);
+      log.info("AsyncSdkTimeoutCallback notify is called for ambiance with nodeExecutionId {}",
+          AmbianceUtils.obtainCurrentRuntimeId(ambiance));
+      sdkNodeExecutionService.resumeNodeExecution(ambiance, responseMap, false, timedOut);
+    } catch (InvalidProtocolBufferException e) {
+      log.error("Not able to deserialize Ambiance bytes. Progress Callback will not be executed");
+    }
   }
 }
