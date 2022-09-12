a8fa0c44ce4ff1ee744fb15a61b0ae7b6fdad2c8
vivekkarnatiharness <vivek.karnati@harness.io>
2022-09-12 22:18:28 +0530

    [CDS-42886]: Regions

--- 120-ng-manager/src/main/java/io/harness/ng/core/artifacts/resources/gar/GARArtifactResource.java
+++ 120-ng-manager/src/main/java/io/harness/ng/core/artifacts/resources/gar/GARArtifactResource.java
@@ -18,6 +18,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import java.util.ArrayList;
+import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
